053ecf7819121f086ab6f26edefb24916689997d
ken-paterson <152591@curric.local>
2017-05-27 14:59:58 +0100

    simple script

---
+++ cc.py
@@ -0,0 +1,12 @@
+
+exR = float(input("enter exhange rate "))
+
+gdp = float(input("enter amount of holiday money "))
+EU = gdp * exR
+
+print (EU)
+
+if EU < 500 :
+    print ("you cannot go on holiday")
+else:
+    print ("Holiday!")
