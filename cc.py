40f32ac67252446987e72175cb4b7da7d60ab9cf
user <kpaterson@runbox.com>
2017-07-30 23:46:52 +0100

    cc change 2

--- cc.py
+++ cc.py
@@ -7,6 +7,6 @@ EU = gdp * exR
 print (EU)
 
 if EU < 500 :
-    print ("you cannot go on holiday yet but soon")
+    print ("you cannot go on holiday yet but soon, soon!")
 else:
     print ("Holiday!")
